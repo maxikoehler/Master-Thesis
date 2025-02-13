c79f361b0db8e65123ce4e1a469b768fec5fb63f
maxikoehler <64246754+maxikoehler@users.noreply.github.com>
2025-02-13 22:37:40 +0100

    added clear_files.py for tidy workspace

---
+++ clear_files.py
@@ -0,0 +1,39 @@
+import os
+
+endings = [
+    '.gz',
+    '.xml',
+    '.bbl',
+    '.atoc',
+    '.aux',
+    '.log',
+]
+
+def remove_files(dir_name, extensions):
+    dirs = []
+    for file in os.listdir(dir_name):
+        file_name, file_extension = os.path.splitext(file)
+        if file_extension == "" and file_name[0] != ".":
+            dirs.append(file_name)
+
+        if file_extension in extensions:
+            print(file)
+            file_path = os.path.join(dir_name, file)
+            os.remove(file_path)
+
+    for d in dirs:
+        new_dir = os.path.join(dir_name, d)
+        remove_files(new_dir, extensions)
+
+    return
+
+def main():
+    print("Clearing files...")
+
+    dir_name = os.getcwd()
+    remove_files(dir_name, endings)
+
+    print("Unnecessary files cleared.")
+
+if __name__ == "__main__":
+    main()
